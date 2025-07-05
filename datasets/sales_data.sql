7509f47ec8a1a82db634fefad4af70ebb9cac6f5
RAKESH333-IGTM <rakeshchoutpally666@gmail.com>
2025-07-05 17:30:12 +0530

    Update sales_data.sql

--- datasets/sales_data.sql
+++ datasets/sales_data.sql
@@ -1,1 +1,20 @@
+--------=====================================================================----------
+-----------SALES TABLE CREAIION IN VIEWS-------------------------------------------
+--------=====================================================================----------
 
+CREATE VIEW gold.fact_sales AS
+	SELECT 
+		sd.sls_ord_num order_number,
+		pd.product_key,
+		cd.customer_key,
+		sd.sls_order_dt order_date,
+		sd.sls_ship_dt shipping_date,
+		sd.sls_due_dt due_date,
+		sd.sls_sales sales_amount,
+		sd.sls_quantity quantity,
+		sd.sls_price price
+	FROM silver.crm_sales_details sd
+	LEFT JOIN gold.dim_products pd
+	ON sd.sls_prd_key = pd.product_number
+	LEFT JOIN gold.dim_customers cd
+	ON sd.sls_cust_id = cd.customer_id
