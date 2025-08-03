8c187c5f81e14baef220b1f48029d70172d1ed54
RAKESH333-IGTM <rakeshchoutpally666@gmail.com>
2025-08-03 20:35:17 +0530

    Update 07.change_over_time_analysis.sql

--- EDA.Scripts/07.change_over_time_analysis.sql
+++ EDA.Scripts/07.change_over_time_analysis.sql
@@ -4,11 +4,13 @@
 ---Analysed through measure by date_dimension---
 ---===================================================---
 
---Analyse sales performance over time
 
+-----------------------------------------------------
+---------Analyse sales performance over time------
+-----------------------------------------------------
 SELECT 
-    DATETRUNC(year,order_date) sales_year,---month wise sales analysis with date data_type
-	--month(order_date)  ---Year wise sales analysis with int data_type
+    	DATETRUNC(year,order_date) sales_year,            ---month wise sales analysis with date data_type
+	--month(order_date)                               ---Year wise sales analysis with int data_type
 	SUM(sales_amount) Total_sales,
 	SUM(quantity) Total_quantity,  
 	COUNT(customer_key) customer_count
